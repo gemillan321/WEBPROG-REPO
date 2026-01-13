7270d75486f03c3716da842ccff9f74ad9611b29
gemillan321 <144914936+gemillan321@users.noreply.github.com>
2026-01-13 07:34:02 +0000

    implemented vue

---
+++ firstfc/src/App.vue
@@ -0,0 +1,32 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
+
+App.vue
+ 
+<template>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+</template>
+ 
+<script></script>
+ 
+<style>
+ #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
